10c5e14e602e732711f472ef4c04c497a9168af5
amethyste <javiahernandez67@gmail.com>
2025-04-03 13:02:27 -0600

    añadiendo ejemplos de conteo con for y while

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert("Hola, mundo!");
+// Ejemplo de uso de for y while en JavaScript
+// 1. Contar hasta 10 usando for
+for (let i = 1; i <= 10; i++) {
+    console.log(i); // Imprime los números del 1 al 10
+}
